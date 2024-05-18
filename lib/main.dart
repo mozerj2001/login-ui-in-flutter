61bed4fe4fb529ea6c5fead93659735d304cccc2
Jozsef Mozer <mozerj2001@gmail.com>
2024-05-18 16:27:17 +0200

    login page UI widget structure completed

--- lib/main.dart
+++ lib/main.dart
@@ -48,62 +48,7 @@ class MyApp extends StatelessWidget {
       navigatorObservers: GetIt.I<List<NavigatorObserver>>(),
       //DO NOT MODIFY
       debugShowCheckedModeBanner: false,
-      home: MyLoginPage(),
-    );
-  }
-}
-
-class MyLoginPage extends StatelessWidget {
-  final _key = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _loginPrompt(),
-    );
-  }
-
-  Widget _loginPrompt() {
-    return Form(
-        key: _key,
-        child: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          _emailField(),
-          _passwordField(),
-          _loginButton(),
-        ])));
-  }
-
-  Widget _emailField() {
-    return TextFormField(
-      validator: (value) => null,
-      obscureText: false,
-      decoration: InputDecoration(
-        icon: Icon(Icons.person),
-        hintText: 'EMAIL',
-      ),
-    );
-  }
-
-  Widget _passwordField() {
-    return TextFormField(
-      validator: (value) => null,
-      obscureText: true,
-      decoration: InputDecoration(
-        icon: Icon(Icons.security),
-        hintText: 'PWD',
-      ),
-    );
-  }
-
-  Widget _loginButton() {
-    return Padding(
-      padding: const EdgeInsets.all(25.0),
-      child: ElevatedButton(
-        onPressed: () {},
-        child: Text('LOGIN'),
-      ),
+      home: LoginPageBloc(),
     );
   }
 }
--- lib/ui/bloc/login/login_page.dart
+++ lib/ui/bloc/login/login_page.dart
@@ -15,9 +15,75 @@ class _LoginPageBlocState extends State<LoginPageBloc> {
     super.initState();
   }
 
+  final _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError('Page not implemented!');
+    return Scaffold(
+      body: _loginPrompt(),
+    );
+  }
+
+  Widget _loginPrompt() {
+    return Form(
+        key: _key,
+        child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          _emailField(),
+          _passwordField(),
+          _loginButton(),
+          _rememberMeCheckBox(),
+        ])));
+  }
+
+  Widget _emailField() {
+    return TextFormField(
+      validator: (value) => null,
+      obscureText: false,
+      decoration: InputDecoration(
+        icon: Icon(Icons.person),
+        hintText: 'EMAIL',
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return TextFormField(
+      validator: (value) => null,
+      obscureText: true,
+      decoration: InputDecoration(
+        icon: Icon(Icons.security),
+        hintText: 'PWD',
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('LOGIN'),
+      ),
+    );
+  }
+
+  bool _isChecked = false;
+  Widget _rememberMeCheckBox() {
+    return Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Row(children: [
+          Text('Remember login?'),
+          Checkbox(
+            value: _isChecked,
+            onChanged: (value) {
+              setState(() {
+                _isChecked = value!;
+              });
+            },
+          ),
+        ]));
   }
 
   @override
